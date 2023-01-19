d0830bf8887ef9f3424f382ba49dc7a64c149497
marto <marton.szak@gmail.com>
2023-01-19 15:41:31 +0100

    fix enemy strength

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Ghost.java
@@ -4,7 +4,7 @@ import com.codecool.dungeoncrawl.data.Cell;
 
 public class Ghost extends Actor {
     public static final int GHOST_HEALTH = 20;
-    public static final int GHOST_STRENGTH = 7;
+    public static final int GHOST_STRENGTH = 10;
     public Ghost(Cell cell) {
         super(cell, GHOST_HEALTH, GHOST_STRENGTH, true);
     }
--- src/main/java/com/codecool/dungeoncrawl/data/actors/GiantSkeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/GiantSkeleton.java
@@ -4,7 +4,7 @@ import com.codecool.dungeoncrawl.data.Cell;
 
 public class GiantSkeleton extends Actor{
     public static final int GIANT_SKELETON_HEALTH = 30;
-    public static final int GIANT_SKELETON_STRENGTH = 5;
+    public static final int GIANT_SKELETON_STRENGTH = 15;
     public GiantSkeleton(Cell cell) {
         super(cell, GIANT_SKELETON_HEALTH, GIANT_SKELETON_STRENGTH, false);
     }
